6bd38687dee661e1d8146eaba85c3f9ba207898a
sergei.doroshenko <6823298@gmail.com>
2016-11-04 16:30:05 +0300

    Enhanced jsr352 example.

--- jsr352-se/src/main/java/org/sergei/batch/BatchApp.java
+++ jsr352-se/src/main/java/org/sergei/batch/BatchApp.java
@@ -3,6 +3,11 @@ package org.sergei.batch;
 import javax.batch.operations.JobOperator;
 import javax.batch.runtime.BatchRuntime;
 import javax.batch.runtime.JobExecution;
+import javax.batch.runtime.Metric;
+import javax.batch.runtime.StepExecution;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -14,16 +19,16 @@ public class BatchApp {
     }
 
     public static void runJob ( String jobName ) {
-        JobOperator operator = BatchRuntime.getJobOperator();
+        JobOperator jobOperator = BatchRuntime.getJobOperator();
 
         Properties properties = new Properties();
         properties.setProperty( "file.in", "batchFiles/in/words.txt" );
         properties.setProperty( "file.out", "batchFiles/out/output.txt" );
 
-        long jobId = operator.start( jobName, properties );
+        long executionId = jobOperator.start( jobName, properties );
 
         // Monitor the job
-        JobExecution execution = operator.getJobExecution( jobId );
+        JobExecution execution = jobOperator.getJobExecution( executionId );
 
         System.out.println( "****************************************************************" );
         System.out.println( "**             Job name: " + execution.getJobName() );
@@ -31,8 +36,11 @@ public class BatchApp {
         System.out.println( "**             Job start time: " + execution.getStartTime() );
         System.out.println( "****************************************************************" );
 
+
+
         try {
             Thread.sleep( 1000 * 10 );
+
             if ( execution.getExitStatus().equals( "COMPLETED" ) ) {
 
                 System.out.println( "****************************************************************" );
@@ -40,10 +48,32 @@ public class BatchApp {
                 System.out.println( "**             Job exit status: " + execution.getExitStatus() );
                 System.out.println( "****************************************************************" );
 
+                List<StepExecution> stepExecutions = jobOperator.getStepExecutions( executionId );
+
+                for ( StepExecution stepExecution : stepExecutions ) {
+
+                    System.out.println( "**             Step name: " + stepExecution.getStepName() );
+
+                    Map<Metric.MetricType, Long> metricsMap = getMetricsMap( stepExecution.getMetrics() );
+
+                    System.out.println( "**             Read count: " + metricsMap.get( Metric.MetricType.READ_COUNT ).longValue() );
+                    System.out.println( "**             Write count: " + metricsMap.get( Metric.MetricType.WRITE_COUNT ).longValue() );
+                    System.out.println( "**             Commit count:" + metricsMap.get( Metric.MetricType.COMMIT_COUNT ).longValue() );
+                }
+
+
                 System.exit( 0 );
             }
         } catch ( InterruptedException e ) {
             e.printStackTrace();
         }
+    }
+
+    private static Map<Metric.MetricType, Long> getMetricsMap ( Metric[] metrics ) {
+        Map<Metric.MetricType, Long> metricsMap = new HashMap<>();
+        for ( Metric metric : metrics ) {
+            metricsMap.put( metric.getType(), metric.getValue() );
+        }
+        return metricsMap;
     }
 }
