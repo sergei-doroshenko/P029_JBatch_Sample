850ed023ca022c06220c58f377f321415a109580
sergei.doroshenko <6823298@gmail.com>
2016-11-10 15:19:25 +0300

    Enhanced LaunchApp.

--- jsr352-se/src/main/java/org/sergei/batch/BatchApp.java
+++ jsr352-se/src/main/java/org/sergei/batch/BatchApp.java
@@ -11,14 +11,14 @@ import java.util.*;
  */
 public class BatchApp {
 
-    public static void main ( String[] args ) {
+    public static void main ( String[] args ) throws InterruptedException {
         if ( args.length == 0 ) {
-            throw new IllegalStateException("Define job name and parameters.");
+            throw new IllegalStateException( "Define job name and parameters." );
         }
         runJob( args[0] );
     }
 
-    public static void runJob ( String jobName ) {
+    public static void runJob ( String jobName ) throws InterruptedException {
         JobOperator jobOperator = BatchRuntime.getJobOperator();
 
         Properties properties = new Properties();
@@ -34,24 +34,21 @@ public class BatchApp {
         long executionId = jobOperator.start( jobName, properties );
         JobExecution execution = jobOperator.getJobExecution( executionId );
 
-        try {
+        while ( execution.getBatchStatus().equals( BatchStatus.STARTED ) ) {
             Thread.sleep( 100 * 10 );
+        }
 
-            if ( execution.getExitStatus() != null && execution.getExitStatus().equals( "COMPLETED" ) ) {
+        if ( execution.getBatchStatus().equals( BatchStatus.COMPLETED ) ) {
+            System.out.println( BatchUtils.SEPARATOR );
+            System.out.printf( BatchUtils.STR_FORMAT, "Job end time", BatchUtils.DATE_FORMAT.format( execution.getEndTime() ) );
+            System.out.printf( BatchUtils.STR_FORMAT, "Job exit status", execution.getExitStatus() );
+            System.out.println( BatchUtils.SEPARATOR );
 
-                System.out.println( BatchUtils.SEPARATOR );
-                System.out.printf( BatchUtils.STR_FORMAT, "Job end time", BatchUtils.DATE_FORMAT.format(execution.getEndTime()) );
-                System.out.printf( BatchUtils.STR_FORMAT, "Job exit status",  execution.getExitStatus() );
-                System.out.println( BatchUtils.SEPARATOR );
+            List<StepExecution> stepExecutions = jobOperator.getStepExecutions( executionId );
 
-                List<StepExecution> stepExecutions = jobOperator.getStepExecutions( executionId );
+            stepExecutions.forEach( stepExecution -> printMetrics( stepExecution ) );
 
-                stepExecutions.forEach( stepExecution -> printMetrics( stepExecution ) );
-
-                System.exit( 0 );
-            }
-        } catch ( InterruptedException e ) {
-            e.printStackTrace();
+            System.exit( 0 );
         }
     }
 
@@ -62,7 +59,7 @@ public class BatchApp {
         return metricsMap;
     }
 
-    private static void printMetrics(StepExecution stepExecution) {
+    private static void printMetrics ( StepExecution stepExecution ) {
 
         System.out.printf( BatchUtils.STR_FORMAT, "Step name", stepExecution.getStepName() );
 
@@ -80,7 +77,7 @@ public class BatchApp {
     private static void printPreviousExecutions ( JobOperator jobOperator ) {
         List<JobInstance> jobInstances = jobOperator.getJobInstances( "sampleJob", 0, 1000 );
         jobInstances.forEach( i -> jobOperator.getJobExecutions( i )
-                .forEach( ex -> System.out.println("id: " + ex.getExecutionId() + ", status: " + ex.getExitStatus() ) ) );
+                .forEach( ex -> System.out.println( "id: " + ex.getExecutionId() + ", status: " + ex.getExitStatus() ) ) );
 
         /*List<Long> runningExecutions = jobOperator.getRunningExecutions( "sampleJob" );
         runningExecutions.forEach( e -> System.out.println(e) );*/
